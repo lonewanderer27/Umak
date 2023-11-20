412498e3d4c7f74ade39456cb0f047652b7c7521
NeroXerith <astar8820@gmail.com>
2023-11-20 20:07:25 +0800

    First commit, Init files

---
+++ app/src/main/java/com/bryle_sanico/umak/MainActivity.java
@@ -0,0 +1,15 @@
+package com.bryle_sanico.umak;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.material.button.MaterialButton;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
